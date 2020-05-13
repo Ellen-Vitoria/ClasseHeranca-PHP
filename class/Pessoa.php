0acca5536427a48f580f64f4666439d9e5a8e0fa
ellen <ellen.vitoria.ogs@gmail.com>
2020-05-13 17:06:42 -0300

    Exibindo o Atributo Cpf

--- class/Pessoa.php
+++ class/Pessoa.php
@@ -1,11 +1,29 @@
 <?php
 
+// Parâmetro = Argumento
 class Pessoa
 {
     public $cpf;
 
     public __construct($cpf) // Método da classe no PHP - Inicializar qualquer Objeto
     {
+        validarIdade(); // Executar antes de inderir Cpf - Chamando Método dentro da própria Classe
+        validarCpf();
         $this->cpf = $cpf;
+    }
+
+    /* 
+       Se fosse "public __construct($cpf = null)", estaria declarando que esse Parâmetro é opcional
+       Ou
+       "public __construct($cpf, $idade = 18)", o primeiro Parâmetro seria obrigatório, mas o segundo não
+    */
+
+    public function validarIdade()
+    {
+        return true;
+    }
+    public function validarCpf()
+    {
+        return true;
     }
 }
--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 
 require_once __DIR__ . '/class/Pessoa.php'; // __DIR__: não precisar colocar o caminho inteiro
 
-$pessoa = new Pessoa();
+$pessoa = new Pessoa("047.721.538-61");
+echo "<p>CPF da Pessoa: {$pessoa->cpf}</p>"; // Com as Chaves, é separado o código Php do código HTML
+$pessoa->validarCpf(); // Chamando Método de forma externa
